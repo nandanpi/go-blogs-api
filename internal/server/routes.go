d6188c0516b554e1ee821bcf30f5439f18c4a054
Nandan R Pai <nandanrp09@gmail.com>
2024-07-02 21:59:31 +0530

    [update] Allow all origins

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -3,12 +3,24 @@ package server
 import (
 	"go-blog-api/internal/auth"
 	"net/http"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := gin.Default()
+
+	config := cors.Config{
+		AllowAllOrigins: true,
+
+		AllowMethods:     []string{"POST", "GET", "OPTIONS", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}
 
 	r.GET("/", auth.ProtectedRoute(s.RootRouteHandler))
 	r.POST("/signup", s.HandleSignUp)
@@ -24,6 +36,8 @@ func (s *Server) RegisterRoutes() http.Handler {
 	r.DELETE("deleteComment/:id", auth.ProtectedRoute(s.DeleteCommentHandler))
 	r.POST("/likePost", auth.ProtectedRoute(s.LikePostHandler))
 	r.POST("/unlikePost", auth.ProtectedRoute(s.UnlikePostHandler))
+
+	r.Use(cors.New(config))
 
 	return r
 }
